053f39d2db72ed01b39553c1f225d99220cd78cc
ashwinithebest <71215868+ashwinithebest@users.noreply.github.com>
2024-08-13 18:24:36 +0530

    #2 langchain integration

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # AI integrated E-Commerce web App LangChain 
-This Project is full stack E-Comm web app which uses Azure Open AI LLM.
-Tech-Stack 
-Langchain 
-Azure OpenAI 
-React JavaScript Frontend
-Flask Python Backend
+This Project is full stack E-Comm web app which uses Azure Open AI LLM.  
+Tech-Stack   
+Langchain   
+Azure OpenAI   
+React JavaScript Frontend  
+Flask Python Backend  
+
+
+### This project demonstrates how to interact with Azure OpenAI using Python. It includes a function to get completions from the Azure OpenAI API. 
 
--- src/main.py
+++ src/main.py
@@ -1,4 +1,37 @@
+from dotenv import load_dotenv, find_dotenv
 from http_request import get_completion
+from langchain_openai import AzureChatOpenAI
+import os
+
+# Load environment variables from .env file
+load_dotenv(find_dotenv()) 
+
+# Set environment variables
+endpoint = os.getenv("OPENAI_ENDPOINT")
+api_key = os.getenv("OPENAI_API_KEY")
+os.environ["AZURE_OPENAI_API_KEY"] = api_key
+os.environ["AZURE_OPENAI_ENDPOINT"] =endpoint 
+
+
+llm = AzureChatOpenAI(
+    azure_deployment="gpt-35-turbo", 
+    api_version="2024-04-01-preview", 
+    temperature=0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+   
+)
+messages = [
+    (
+        "system",
+        "You are a helpful assistant.",
+    ),
+    ("human", "who won last IPL "),
+]
+ai_msg = llm.invoke(messages)
+print(ai_msg.content)
+
 # Example usage
-response = get_completion("Who won the football world cup in 2020?")
-print(response)
+# response = get_completion("tell me table of 5")
+# print(response)
